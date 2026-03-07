32ceaa3a3dc74ead75f3f96ea67281bdd32d3672
Soni <guptasoni30461@gmail.com>
2026-03-07 16:14:02 +0530

    Refactor authMiddleware to prioritize cookie token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,53 +1,9 @@
-// import jwt from "jsonwebtoken";
-// import User from "../models/User.js";
-
-// export const protect = async (req, res, next) => {
-//   try {
-//     const token = req.cookies.token;
-
-//     if (!token) {
-//       return res.status(401).json({ message: "Not authorized, no token" });
-//     }
-
-//     // Verify token
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     // Find user from DB
-//     const user = await User.findById(decoded.id).select("-password");
-
-//     if (!user) {
-//       return res.status(401).json({ message: "User not found" });
-//     }
-
-//     // Attach user to request
-//     req.user = user;
-
-//     next();
-
-//   } catch (error) {
-//     return res.status(401).json({ message: "Not authorized, token failed" });
-//   }
-// };
-
-
-
-
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const protect = async (req, res, next) => {
   try {
-    let token;
-
-  
-    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-      token = req.headers.authorization.split(" ")[1];
-    }
-
-   
-    if (!token && req.cookies.token) {
-      token = req.cookies.token;
-    }
+    const token = req.cookies.token;
 
     if (!token) {
       return res.status(401).json({ message: "Not authorized, no token" });
@@ -72,3 +28,6 @@ export const protect = async (req, res, next) => {
     return res.status(401).json({ message: "Not authorized, token failed" });
   }
 };
+
+
+
