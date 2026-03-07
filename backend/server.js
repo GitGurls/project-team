7956e6e70cb136a0b0b66d89f3a2d18666ecace4
Soni <guptasoni30461@gmail.com>
2026-03-07 15:14:56 +0530

    Uncomment server setup and update CORS configuration
    Refactored server.js to uncomment and update CORS settings.

--- backend/server.js
+++ backend/server.js
@@ -1,65 +1,4 @@
 
-
-// import express from "express";
-// import http from "http";
-// import { Server } from "socket.io";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import cookieParser from "cookie-parser";
-
-// import connectDB from "./config/db.js";
-// import authRoutes from "./routes/authRoutes.js";
-// import taskRoutes from "./routes/taskRoutes.js";
-
-// dotenv.config();
-
-// connectDB();
-
-// const app = express();
-// const server = http.createServer(app);
-
-// // middleware
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-
-// app.use(express.json());
-// app.use(cookieParser());
-
-// // routes
-// app.use("/api/auth", authRoutes);
-// app.use("/api/tasks", taskRoutes);
-
-// app.get("/", (req, res) => {
-//   res.send("API Running...");
-// });
-
-// // socket setup
-// export const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   },
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("User connected:", socket.id);
-
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected:", socket.id);
-//   });
-// });
-
-// const PORT = process.env.PORT || 5000;
-
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
@@ -83,7 +22,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:5174"
+      "http://localhost:5174",
+       process.env.FRONTEND_URL
     ],
     credentials: true
   })
@@ -105,7 +45,8 @@ export const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "http://localhost:5174"
+      "http://localhost:5174",
+       process.env.FRONTEND_URL
     ],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true
